414717b6bade6bc2f07f3b3f4cf036de84fa94ac
Micah Coldiron <mcc4339@rit.edu>
2024-01-20 14:15:43 -0800

    Started work on the hw

---
+++ HW3/index.js
@@ -0,0 +1,5 @@
+const first = document.getElementById("intro");
+const second = document.getElementById("newacc");
+const third = document.getElementById("login");
+second.disabled = true;
+third.disabled = true;
